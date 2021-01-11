b869d9d3bd73730eb64d9706e565709e21218f6c
sprinzs123 <sprinzs351@gmail.com>
2021-01-10 21:30:26 -0500

    added state to change navbar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,29 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "./NavBar.css";
 import "./index.css";
 
-function NavBar(){
-    return(
-        <nav className=''>
-        <div className="nav-items">
-            <div className="log-nav">
-                <h1>Logo</h1>
+
+class ShowNav extends Component{
+    constructor(){
+        super()
+        this.state = {
+            showModal: false
+        }
+    }   
+
+    changeView(){
+        console.log('state.showModal')
+    }
+
+
+    render(){
+        return(
+            <nav className=''>
+            <div className="nav-items">
+                <div className="log-nav">
+                    <h1>Logo</h1>
+                </div>
+                <ul className="nav-links">
+                    <li>Virtual Tours</li>
+                    <li>Book Your Tour</li>
+                    <li>(874) 204-1589</li>
+                </ul>
             </div>
-            <ul className="nav-links">
-                <li>Virtual Tours</li>
-                <li>Book Your Tour</li>
-                <li>(874) 204-1589</li>
-            </ul>
-        </div>
-        <div className="nav-toggle">
-            <ul className="nav-btn">
-                <li>LEASE NOW</li>
-                <li>X</li>
-            </ul>
-        </div>
-    </nav>
-    )
+            <div className="nav-toggle">
+                <ul className="nav-btn">
+                    <li>LEASE NOW</li>
+                    <li onClick={this.changeView} className='toggle-btn'>X</li>
+                </ul>
+            </div>
+        </nav>
+        )
+    }
 }
 
 
-export default NavBar;
+
+
+export default ShowNav;
