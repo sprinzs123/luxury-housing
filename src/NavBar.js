43acedb30f73e32606016a90fd80f22002efb96e
sprinzs123 <sprinzs351@gmail.com>
2021-01-11 10:43:19 -0500

    navbar toggle works/ needed new parent div

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,43 +3,80 @@ import "./NavBar.css";
 import "./index.css";
 
 
-class ShowNav extends Component{
-    constructor(){
+class ShowNav extends Component {
+    constructor() {
         super()
         this.state = {
             showModal: false,
-            toggleButton : 'X'
-        }
-    }   
-
-    changeView =() => {
-        this.setState({
             toggleButton: 'Y'
-        })
-        console.log(this.state.toggleButton)
+        }
     }
 
 
-    render(){
-        return(
-            <nav className=''>
-            <div className="nav-items">
-                <div className="log-nav">
-                    <h1>Logo</h1>
+    changeView = () => {
+        const toggleState = this.state.toggleButton
+        const notNavBar = document.querySelector('.content')
+        const modal = document.querySelector('.nav-modal')
+
+        if (toggleState === 'X') {
+            this.setState({
+                toggleButton: 'Y'
+            })
+            notNavBar.style.display = 'inline'
+            modal.style.display = 'none'
+
+        }
+        if (toggleState === 'Y') {
+            this.setState({
+                toggleButton: 'X'
+            })
+            notNavBar.style.display = 'none'
+            modal.style.display = 'inline'
+            console.log(modal)
+
+
+        }
+
+    }
+
+
+    render() {
+        return (
+            <div className='nav-bar-all'>
+                <nav className=''>
+                    <div className="nav-items">
+                        <div className="log-nav">
+                            <h1>Logo</h1>
+                        </div>
+                        <ul className="nav-links">
+                            <li>Virtual Tours</li>
+                            <li>Book Your Tour</li>
+                            <li>(874) 204-1589</li>
+                        </ul>
+                    </div>
+                    <div className="nav-toggle">
+                        <ul className="nav-btn">
+                            <li>LEASE NOW</li>
+                            <li onClick={this.changeView} className='toggle-btn'>{this.state.toggleButton}</li>
+                        </ul>
+                    </div>
+                </nav>
+                <div className='nav-modal'>
+                <div className='nav-modal-flex'>
+                    <ul className='modal-text'>
+                        <li>Home</li>
+                        <li>Amenities</li>
+                        <li>Neighborhood</li>
+                        <li>Gallery</li>
+                        <li>Residents</li>
+                        <li>Contact</li>
+                    </ul>
                 </div>
-                <ul className="nav-links">
-                    <li>Virtual Tours</li>
-                    <li>Book Your Tour</li>
-                    <li>(874) 204-1589</li>
-                </ul>
+
+                </div>
+
+                
             </div>
-            <div className="nav-toggle">
-                <ul className="nav-btn">
-                    <li>LEASE NOW</li>
-                    <li onClick={this.changeView} className='toggle-btn'>{ this.state.toggleButton }</li>
-                </ul>
-            </div>
-        </nav>
         )
     }
 }
