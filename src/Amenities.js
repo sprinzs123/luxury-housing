82b5dc4c393f13eb7da0a7a255247b8c75fe2400
sprinzs123 <sprinzs351@gmail.com>
2021-01-16 21:48:33 -0500

    sliding window animation

--- src/Amenities.js
+++ src/Amenities.js
@@ -1,50 +1,100 @@
-import './amenities.css';
-import React, { Component } from 'react'
+import "./amenities.css";
+import React, { Component } from "react";
 
 export default class Amenities extends Component {
-    constructor(props) {
-        super(props)        
-        this.state = {
-             hidden : true,
-             btnOne: 'show more'
-        }
-    }
-    
-    btnToggle = () => {
-        let hiddenMenu = document.querySelector('.hidden-amenities')
-        if(this.state.btnOne === 'show more'){
-            hiddenMenu.classList.toggle('height-0')
-            this.setState({
-                btnOne : 'show less'
-            })
+	constructor(props) {
+		super(props);
+		this.state = {
+			hidden: true,
+			btnOne: "SHOW MORE",
+		};
+	}
 
-        } else {
-            hiddenMenu.classList.toggle('height-0')
-            this.setState({
-                btnOne : 'show more'
-            })
-        }
-    }
+	btnToggle = () => {
+		let hiddenMenu = document.querySelector(".hidden-amenities");
+		if (this.state.btnOne === "SHOW MORE") {
+			hiddenMenu.classList.toggle("height-0");
+			this.setState({
+				btnOne: "SHOW LESS",
+			});
+		} else {
+			hiddenMenu.classList.toggle("height-0");
+			this.setState({
+				btnOne: "SHOW MORE",
+			});
+		}
+	};
 
-    render() {
-        return (
-            <div className='container'>
-                <div className='all-amenities'>
-                    <div className='show-amenities'>
-                        <ul className='hide'>
-                            <li>Test</li>
-                            <li>test2</li>
-                        </ul>
-                    </div>
-                    <div className='hidden-amenities height-0'>
-                        <ul>
-                            <li>hidden</li>
-                            <li>hidden2</li>
-                        </ul>
-                    </div>
-                    <button onClick={this.btnToggle} className='mt-4'>{this.state.btnOne}</button>
-                </div>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="container mt-4">
+				<div className="all-amenities">
+					<div className="show-amenities">
+						<ul className="amenities-items">
+                            <li className="first-item">
+								 Indoor/Outdoor Lounge
+							</li>                            
+                            <li className="first-item">
+								 Indoor/Outdoor Lounge
+							</li>
+                            <li className="first-item">
+								 Indoor/Outdoor Lounge
+							</li>
+                            <li className="first-item">
+								 Indoor/Outdoor Lounge
+							</li>
+                            <li className="second-item">
+								Indoor/Outdoor Lounge
+							</li>
+                            <li className="second-item">
+								Indoor/Outdoor Lounge
+							</li>                          
+                            <li className="second-item">
+								Indoor/Outdoor Lounge
+							</li>                           
+							<li className="second-item">
+								Indoor/Outdoor Lounge
+							</li>                            
+						</ul>
+					</div>
+					<div className="hidden-amenities">
+						<ul className="amenities-items">
+                            <li className="first-item">
+								 Indoor/Outdoor Lounge
+							</li>                            
+                            <li className="first-item">
+								 Indoor/Outdoor Lounge
+							</li>
+                            <li className="first-item">
+								 Indoor/Outdoor Lounge
+							</li>
+                            <li className="first-item">
+								 Indoor/Outdoor Lounge
+							</li>
+                            <li className="second-item">
+								Indoor/Outdoor Lounge
+							</li>
+                            <li className="second-item">
+								Indoor/Outdoor Lounge
+							</li>                          
+                            <li className="second-item">
+								Indoor/Outdoor Lounge
+							</li>                           
+							<li className="second-item">
+								Indoor/Outdoor Lounge
+							</li>                            
+						</ul>
+					</div>
+					<div className="d-flex justify-content-center">
+						<button
+							onClick={this.btnToggle}
+							className="btn-green mt-4"
+						>
+							{this.state.btnOne}
+						</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
