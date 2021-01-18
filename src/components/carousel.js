94c4dd06b5c65827ebc4097c23fc1df7a59407c5
sprinzs123 <sprinzs351@gmail.com>
2021-01-18 14:52:29 -0500

    carousel working

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -27,7 +27,6 @@ export default class carousel extends Component {
 
     slideBack = () =>{
         const currentIndex = this.state.currentImg
-        console.log('back')
         if(currentIndex === 0){
             this.setState({
                 currentImg : 2
@@ -39,13 +38,35 @@ export default class carousel extends Component {
             })
         }
     }
+
+    slideForwards = () =>{
+        const currentIndex = this.state.currentImg
+        if(currentIndex === 2){
+            this.setState({
+                currentImg : 0
+            })
+        } else {
+            const newImage = currentIndex + 1
+            this.setState({
+                currentImg : newImage
+            })
+        }
+    }
+
+    automaticScrolling = () =>{
+        setTimeout(this.slideForwards, 10000)
+        console.log('changed')
+    }
     
     render() {
         return (
             <div>   
                 <div className="carousel-parent">
                 <h1 onClick={this.slideBack} className='left-slider text-success'>x</h1>
+                <h1 onClick={this.slideForwards} className='right-slider text-success'>x</h1>
+
                 <div className="img-row">
+                    {this.automaticScrolling()}
                     {this.iterateImages()}
                 </div>    
                 </div>
