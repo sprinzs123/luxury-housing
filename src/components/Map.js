4605a13410eef3973aabd263d35f3a8f3ac82d88
sprinzs123 <sprinzs351@gmail.com>
2021-01-28 08:52:34 -0500

    api key works now needed to put it in right location

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,7 +5,7 @@ function Map() {
     return (
         <GoogleMap 
             defaultZoom={10}
-            defaultCenter={{ lat:122.4194, lng:37.7749 }}
+            defaultCenter={{ lat: 45.42, lng: -75.697189 }}
         />
     )
 }
@@ -16,8 +16,9 @@ const WrappedMap = withScriptjs(withGoogleMap(Map))
 export default function App() {
     return  (
         <div>
+            <h1>{`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}</h1>
            <WrappedMap
-                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`}
                 loadingElement={<div style={{ height: `100%` }} />}
                 containerElement={<div style={{ height: `400px` }} />}
                 mapElement={<div style={{ height: `100%` }} />}
